e174345df58fd35f61e34d1126ba449a319abfcc
Joshua Levine <joshlevine@berkeley.edu>
2020-02-15 11:41:02 -0800

    Initial Commit

---
+++ SafeRoute/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeRoute
+//
+//  Created by Josh Levine on 2/15/20.
+//  Copyright © 2020 Josh Levine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
